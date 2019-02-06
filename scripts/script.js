d43b1893c40d67dfc6dced099df1590efe34d49e
Tywin <certainvictorythp@thp@gmail.com>
2019-02-06 08:47:35 -0500

    almost forgot to submit

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,19 @@
 $(document).ready(function () {
     $(".table").addClass("available");
     //Begin avail btn show overlay
-    let table = 0;
+    let table = null;
     let heck = false;
+    let name = null;
+    let phone = null;
+    let party = null;
+    let fff = null;
+// $(document).on("click", ".available", function(e){
+//      code block
+//})
+
+// document.addEventListener("click", function(e) {
+
+// });
 
     $(".table").on("click", function (e) {
 
@@ -14,21 +25,56 @@ $(document).ready(function () {
 
         $(e.target).removeClass("available").addClass("selected");
 
-        $(".btn-save").click(function () {
+        fff = $(e.target);
 
+        $(".btn-save").click(function () {
+        name = $(`#name`).val();
+        phone = $(`#phone`).val();
+        party = $(`#party`).val();
             $(".form-overlay").hide();
             if ($(e.target).hasClass("selected")) {
                 $(e.target).removeClass("selected").addClass("reserved").unbind();
                 $(".picker_container").removeClass("no-click");
+                
             }
+            // $(".field-style").val("")
+            
+      $(fff).attr(`name`, `${name}`)
+      $(fff).attr(`phone`, `${phone}`)
+      $(fff).attr(`party`, `${party}`)
+
+    });
         })
         $(".btn-close").on("click", function () {
             $(".form-overlay").hide();
-            $(e.target).removeClass("selected").addClass("available");
+            $(".table").removeClass("selected").addClass("available");
             $(".picker_container").removeClass("no-click");
         });
+        $(document).on("mouseenter", ".reserved", function(e){
+            // if ($(e.target).attr("name") && $(e.target).attr("phone")) {
+              $(e.target).append(`
+              <section class="tooltip">
+                <p>First Name: ${$(e.target).attr("name")}</p>
+                <p>Phone Number: ${$(e.target).attr("phone")}</p>
+                <p>Size of Party: ${$(e.target).attr("party")}</p>
+              </section>
+              `);
+        })
+            $(document).on("mouseleave", ".reserved", function(e) {
+                $(".tooltip").remove();
+              });
+//           });
+//   $(document).on("click", ".table", (e) => {
+//     table = $(e.target);
+//     table.css("background-color", "red");
+//   });
+//   $(document).on("click", ".btn-save", (e) => {
+   
+//   });
 
-    }); //----------end of main function--------
+ 
+
+}); //----------end of main function--------
 
 
 
@@ -36,5 +82,4 @@ $(document).ready(function () {
 
 
 
-
-}) // ----------END OF DOC----------
+ // ----------END OF DOC----------
